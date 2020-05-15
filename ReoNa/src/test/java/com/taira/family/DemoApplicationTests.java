81780e90556773ecb5171f1f70d710cfeee47dbf
pj <jping@aniu.tv>
2020-05-16 00:23:52 +0800

    ReoNa

---
+++ ReoNa/src/test/java/com/taira/family/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taira.family;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
